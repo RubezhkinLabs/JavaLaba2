750d6007089da39fb15c39b7f77aba68d3c395f3
RubezhkinLabs <pavel-ned-03@mail.ru>
2024-03-19 13:39:04 +0300

    modified:   demo/src/main/java/com/example/Main.java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception { // (1+(2+3)) пример примера (простите за тафтологию)
+    public static void main(String[] args) throws Exception { // ((1*2)+((2+3)/1)) пример примера (простите за тафтологию)
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
         FormulaTree tree = new FormulaTree(str);
